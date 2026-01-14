eacb9a9183728626ba56c9bb6715ff7f61093e8e
Buncy01 <hritikm974@gmail.com>
2026-01-14 23:54:37 +0530

    Update smartprocure.py

--- smartprocure.py
+++ smartprocure.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-import plotly.express as px
+#import plotly.express as px
 from openai import OpenAI
 
 client = OpenAI(api_key=["sk-abcdef1234567890abcdef1234567890abcdef12"])  # For Streamlit Cloud
